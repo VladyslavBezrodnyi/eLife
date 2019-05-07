4abd020fd850618b3f8c850313320a80d0bc4cf2
talasimk <47049598+talasimk@users.noreply.github.com>
2019-05-07 23:48:51 +0300

    DataBase

---
+++ eLifeApi/eLifeApi/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace eLifeApi.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ eLifeApi/eLifeApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace eLifeApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
