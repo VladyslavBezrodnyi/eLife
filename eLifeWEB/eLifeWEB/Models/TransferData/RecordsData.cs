021dfa0c4883ac5d74d5406ad8a8c14864fc85a5
Serhii Moskovets <serhii.moskovets@gmail.com>
2019-06-08 01:26:45 +0300

    login in bot

---
+++ eLifeWEB/eLifeWEB/Models/TransferData/RecordsData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eLifeWEB.Models.TransferData
+{
+    public class RecordsData
+    {
+        //public string email { get; set; }
+    }
+}
