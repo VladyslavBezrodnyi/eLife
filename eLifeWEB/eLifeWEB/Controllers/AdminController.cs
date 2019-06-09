138bd4eb4a1c794010e6664f44a33be12c1a9781
vladyslav.bezrodnyi <vladyslav.bezrodnyi@nure.ua>
2019-06-10 00:54:40 +0300

    add admin

---
+++ eLifeWEB/eLifeWEB/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using eLifeWEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eLifeWEB.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            ViewBag.Password = "123456";
+
+
+            return View(db.ClinicAdmins.ToList());
+        }
+    }
+}
