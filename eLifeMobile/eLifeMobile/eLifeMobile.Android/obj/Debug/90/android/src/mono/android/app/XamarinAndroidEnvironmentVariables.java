7762e2df49c8ee0c9311c82fe25333eab7d53225
vladyslav.bezrodnyi <vladyslav.bezrodnyi@nure.ua>
2019-06-09 21:36:53 +0300

    add mobile project

---
+++ eLifeMobile/eLifeMobile/eLifeMobile.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "48fcc62b-12f2-40d3-8c9f-b8bdced87588",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
